3d74d40f8fc3ff1e234be4dcdd910e02379cb0c8
Albin Brolin <crystallizedtester@gmail.com>
2023-05-26 09:07:23 +0200

    initial commit

---
+++ Programmering2KompletteringNummer1/Bilmotor.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Programmering2KompletteringNummer1;
+
+public class Bilmotor
+{
+    public void Start()
+    {
+        Console.WriteLine("Tjohoo! Bilen startar!!");
+    }
+
+
+    public void Stop()
+    {
+        Console.WriteLine("Jahopp, nu stannade bilen");
+    }
+}
